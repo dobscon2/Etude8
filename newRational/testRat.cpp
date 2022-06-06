6db7ae47f5d77b1a7df5ca524a097f38b9e323cf
Connor Dobson <connordobson8@gmail.com>
2022-06-06 13:38:39 +1200

    Adding more test cases
    Adding more test cases

--- newRational/testRat.cpp
+++ newRational/testRat.cpp
@@ -21,12 +21,18 @@ int main(void) {
     Rational stringCon3 = Rational("2.1/2");
     Rational stringCon4 = Rational("2.5/10");
 
+    Rational rat5 = Rational("1/4");
+
+    Rational ratAddition = stringCon + rat5;
+
+    Rational ratAddition2 = stringCon4 + stringCon3;
+
     cout << "String 1/2 = " << stringCon << "\n";
     cout << "String 5/10 = " << stringCon2 << "\n";
     cout << "String 2.1/2 = " << stringCon3 << "\n";
     cout << "String 2.5/10 = " << stringCon4 << "\n";
 
-
-
+    cout << "1/2 + 1/4 = " << ratAddition << "\n";
+    cout << "2.5/10 + 2.1/2 = " << ratAddition2 << "\n";
 
 }
