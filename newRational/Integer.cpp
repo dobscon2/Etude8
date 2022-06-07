cfa3a6ec698c4c13be4f5e8cb066722a1d7cd13b
Connor Dobson <connordobson8@gmail.com>
2022-06-07 15:51:01 +1200

    Added comments
    Added comments to the Integer.cpp file

--- newRational/Integer.cpp
+++ newRational/Integer.cpp
@@ -1,26 +1,52 @@
+/**
+ * @file Integer.cpp
+ * @author Connor Dobson & Ariana van Lith
+ * @brief The integer class is a new data type that can hold an integer bigger than the typical formats built into C++
+ * @version 0.1
+ * @date 2022-06-07
+ * 
+ * 
+ */
+
 #include "Integer.h"
 
 
 namespace cosc326
 {
-    // default constructor
+    /**
+     * @brief Construct a new Integer:: Integer object with the default value of "0"
+     * 
+     */
     Integer::Integer(){
         this->value = "0";
     }
 
-    // Integer Constructor
+    /**
+     * @brief Construct a new Integer from an existing integer variable
+     * 
+     * @param i is the Integer variable that is getting copied to the new variable
+     */
     Integer::Integer(const Integer& i)
     {
         value = parseValue(i.value);
     }
     
-    // String Constructor
+    /**
+     * @brief Construct a new Integer:: Integer object from a string input
+     * 
+     * @param s is the string that is getting parsed into a variable
+     */
     Integer::Integer(const std::string& s)
     {
         value = parseValue(s);
     }
 
 
+    /**
+     * @brief The setValue method changes the current value of the integer from the string input
+     * 
+     * @param val is the string value that is getting reassigned
+     */
     void Integer::setValue(std::string val)
     {
         value = parseValue(std::move(val));
@@ -29,14 +55,23 @@ namespace cosc326
     //Destructor
     Integer::~Integer(){}
 
-    // Assignment Operator
+    /**
+     * @brief This method overrides the = operator and assigns an integer with a new value
+     * 
+     * @param i is the new integer value going to be assigned to the current integer variable
+     * @return returns the address variable of the integer that have been changed
+     */
     Integer& Integer::operator=(const Integer& i)
     {
         this->value = i.value;
         return *this;
     }
 
-    // Negative Assignment
+    /**
+     * @brief This method overirdes the - operator but does not take in any parameters as this converts an integer to a negative value
+     * 
+     * @return the result as an integer
+     */
     Integer Integer::operator-() const
     {
         if (isPositive())
@@ -46,6 +81,13 @@ namespace cosc326
         return Integer(absValue().toString());
     }
 
+    /**
+     * @brief This method overrides the / operator is able to divide between 2 Integer numbers
+     * 
+     * @param lhs the left hand integer
+     * @param rhs the right hand integer
+     * @return the result as an integer
+     */
     Integer operator/(const Integer &lhs, const Integer &rhs)
     {
         if (rhs != ZERO && lhs != ZERO)
@@ -70,6 +112,14 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief The divide method returns 2 integer values, the result of the division and the remainder. We have implemented 
+     * the repeated subraction division method for dividing our values.
+     * 
+     * @param lhs The left hand integer value
+     * @param rhs The right hand integer value
+     * @return A Integer* pointer with 2 Integer values attached to it.
+     */
     Integer *Integer::divide(const Integer &lhs, const Integer &rhs)
     {
         if (rhs != ZERO  && lhs != ZERO)
@@ -110,6 +160,13 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief This method overirdes the * operator and times the 2 values together
+     * 
+     * @param lhs The left hand integer
+     * @param rhs The right hand integer
+     * @return returns the result as an integer 
+     */
     Integer operator*(const Integer &lhs, const Integer &rhs)
     {
         if (lhs.isPositive() && rhs.isPositive())
@@ -127,6 +184,13 @@ namespace cosc326
         return Integer::multiply(lhs.absValue(), rhs.absValue());
     }
 
+    /**
+     * @brief The multiply function times the 2 integers together
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return returns the result as an integer 
+     */
     Integer Integer::multiply(const Integer &lhs, const Integer &rhs)
     {
         if (lhs.isPositive() == true && lhs.isPositive() == true)
@@ -173,6 +237,13 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief This method overirdes the - operator and subtracts the 2 integers
+     * 
+     * @param lhs the left hand side integer 
+     * @param rhs the right hand side integer
+     * @return returns the result as an integer 
+     */
     Integer operator-(const Integer &lhs, const Integer &rhs)
     {
         if (!lhs.isPositive() && !rhs.isPositive())
@@ -200,6 +271,13 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief The diff method calculates the difference between the 2 integers
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return returns the result as an integer 
+     */
     Integer Integer::diff(const Integer &lhs, const Integer &rhs)
     {
         assert(lhs.isPositive() && rhs.isPositive());
@@ -241,6 +319,13 @@ namespace cosc326
         return Integer(str);
     }
 
+    /**
+     * @brief This method overrides the + function and adds the 2 integers together
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return returns the result as an integer 
+     */
     Integer operator+(const Integer &lhs, const Integer &rhs)
     {
         if (lhs.isPositive() && rhs.isPositive())
@@ -258,6 +343,13 @@ namespace cosc326
         return -(lhs.absValue() + rhs.absValue());
     }
 
+    /**
+     * @brief The addition method adds the 2 integers together
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return returns the result as an integer 
+     */
     Integer Integer::addition(const Integer &lhs, const Integer &rhs)
     {
         std::string str1 = lhs.toString();
@@ -287,6 +379,13 @@ namespace cosc326
         return Integer(result);
     }
 
+    /**
+     * @brief This function overrides the % operator and calculates the mod of the 2 integers by getting the remainder from the divide function
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return returns the result as an integer 
+     */
     Integer operator%(const Integer &lhs, const Integer &rhs)
     {
         if (lhs == ZERO || (lhs.absValue() == rhs.absValue()))
@@ -304,6 +403,13 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief The gcd function calculates the greatest common divisor of the 2 integers
+     * 
+     * @param a an integer value
+     * @param b an integer value
+     * @return returns the result as an integer 
+     */
     Integer gcd(const Integer &a, const Integer &b)
     {
         Integer a1 = a.absValue();
@@ -320,38 +426,74 @@ namespace cosc326
         return gcd(b1, remainder);
     }
 
-    
-
+    /**
+     * @brief This function overrides the/= operator and divides the current integer with the other integer passed through
+     * 
+     * @param i the other integer being divided with
+     * @return returns the address of the modified Integer& 
+     */
     Integer &Integer::operator/=(const Integer &i)
     {
         *this = *this / i;
         return *this;
     }
 
+    /**
+     * @brief This function overrides the %= operator and mods the curent integer with the other integer passed through
+     * 
+     * @param i the other integer being mod with
+     * @return returns the address of the modified Integer& 
+     */
     Integer &Integer::operator%=(const Integer &i)
     {
         *this = *this % i;
         return *this;
     }
 
+    /**
+     * @brief This function overrides the *= operator and times the curent integer with the other integer passed through
+     * 
+     * @param i the other integer being times with
+     * @return returns the address of the modified Integer& 
+     */
     Integer &Integer::operator*=(const Integer &i)
     {
         *this = (*this) * i;
         return *this;
     }
 
+    /**
+     * @brief This function overrides the -= operator and subtracts the current integer with the other integer passed through
+     * 
+     * @param i the other integer being subtracted with
+     * @return returns the address of the modified Integer& 
+     */
     Integer &Integer::operator-=(const Integer &i)
     {
         *this = *this - i;
         return *this;
     }
 
+    /**
+     * @brief This function overrides the += operator and adds the current integer with the other integer passed through
+     * 
+     * @param i the other integer being added with
+     * @return returns the address of the modified Integer& 
+     */
     Integer &Integer::operator+=(const Integer &i)
     {
         *this = *this + i;
         return *this;
     }
 
+    /**
+     * @brief This function overrides the < operator and compares if the left hand side is less than the right hand side
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the left hand side is less than the right hand side
+     * @return false if the left hand side is greater than the right hand side
+     */
     bool operator<(const Integer &lhs, const Integer &rhs)
     {
         if (lhs == rhs)
@@ -373,20 +515,53 @@ namespace cosc326
         return !Integer::compare(lhs.absValue(), rhs.absValue());
     }
 
+    /**
+     * @brief This function overrides the > operator and compares if the left hand side is greater than the right hand side
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the left hand side is greater than the right hand side
+     * @return false if the left hand side is less than the right hand side
+     */
     bool operator>(const Integer &lhs, const Integer &rhs)
     {
         return !(lhs <= rhs);
     }
 
+    /**
+     * @brief This function overrides the <= operator and compares if the left hand side is less than or equals the right hand side
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the left hand side is less than or equals the right hand side
+     * @return false if the left hand side is greater than the right hand side
+     */
     bool operator<=(const Integer &lhs, const Integer &rhs)
     {
         return (lhs < rhs) || (lhs == rhs);
     }
 
+    /**
+     * @brief This function overrides the >= operator and compares if the left hand side is greater than or equals the right hand side
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the left hand side is greater or equals the right hand side
+     * @return false if the left hand side is less than the right hand side
+     */
     bool operator>=(const Integer &lhs, const Integer &rhs)
     {
         return (lhs > rhs) || (lhs == rhs);
     }
+
+    /**
+     * @brief The compare function provides the logic for the comparision methods
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the comparision is true
+     * @return false if the comparision is false
+     */
     bool Integer::compare(const Integer &lhs, const Integer &rhs)
     {
         if (lhs.isPositive() == true && rhs.isPositive() == true)
@@ -413,28 +588,60 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief This function overrides the == operator and compares if the left hand side and right hand side equal
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the left hand side and right hand side equal
+     * @return false if the left hand side and right hand side don't equal
+     */
     bool operator==(const Integer &lhs, const Integer &rhs)
     {
         return lhs.toString() == rhs.toString();
     }
 
+    /**
+     * @brief This function overrides the != operator and compares if the left and side and right hand side don't equal
+     * 
+     * @param lhs the left hand side integer
+     * @param rhs the right hand side integer
+     * @return true if the left hand side does't equal the right hand side
+     * @return false if the left hand side equls the right hand side
+     */
     bool operator!=(const Integer &lhs, const Integer &rhs)
     {
         return !(lhs == rhs);
     }
 
+    /**
+     * @brief This function overrides the + operator and converts an integer into a positive value
+     * 
+     * @return return a positive Integer 
+     */
     Integer Integer::operator+() const
     {
         return Integer(value);
     }
 
     
-
+    /**
+     * @brief This function returns a boolean if the integer is positive or not
+     * 
+     * @return true if the integer is positive
+     * @return false if the integer is negative
+     */
     bool Integer::isPositive() const
     {
         return Integer::isStringPositive(value);
     }
 
+    /**
+     * @brief This function parses the string input and removes an excess zeros and positive signs if need be
+     * 
+     * @param val the input string value
+     * @return the result as a std::string 
+     */
     std::string Integer::parseValue(std::string val)
     {
         if (isInteger(val) == true)
@@ -453,6 +660,12 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief This function returns the absolute value of the integer as a string
+     * 
+     * @param str the input string
+     * @return returns a std::string value
+     */
     std::string Integer::strAbsValue(std::string str) const
     {
         if (!isStringPositive(str))
@@ -462,11 +675,23 @@ namespace cosc326
         return str;
     }
 
+    /**
+     * @brief This function returns the absolute value as a Integer
+     * 
+     * @return returns the absolute value as integer 
+     */
     Integer Integer::absValue() const
     {
         return Integer(strAbsValue(value));
     }
 
+    /**
+     * @brief This function detects if the integer string input is a positive value or not
+     * 
+     * @param str the string input
+     * @return true if the string input is a positive value
+     * @return false if the string input is a negative value
+     */
     bool Integer::isStringPositive(std::string str)
     {
         if (!str.empty())
@@ -476,6 +701,13 @@ namespace cosc326
         return false;
     }
 
+    /**
+     * @brief This function detects if the string input is a valid integer
+     * 
+     * @param str the string input
+     * @return true if the string is a integer value
+     * @return false if the string is not an integer value
+     */
     bool Integer::isInteger(std::string str)
     {
         int i = 0;
@@ -494,6 +726,12 @@ namespace cosc326
         return true;
     }
 
+    /**
+     * @brief This function removes the leading zeros from the string input
+     * 
+     * @param str the string input
+     * @return returns a std::string value 
+     */
     std::string Integer::removeLeadingZeros(std::string str)
     {
 
@@ -523,6 +761,12 @@ namespace cosc326
         }
     }
 
+    /**
+     * @brief This function removes the positive sign from the string input
+     * 
+     * @param str the string input
+     * @return returns a std::string value 
+     */
     std::string Integer::removePositiveSign(std::string str)
     {
         if (str[0] == '+')
@@ -539,12 +783,26 @@ namespace cosc326
         return str;
     }
 
+    /**
+     * @brief This function overrides the << operator and sends the integer value to the output stream
+     * 
+     * @param os the output stream
+     * @param i the integer value to output
+     * @return returns a std::ostream& output stream 
+     */
     std::ostream& operator<<(std::ostream& os, const Integer& i)
     {
         os << i.toString() << std::endl;
         return os;
     }
 
+    /**
+     * @brief This function overrides the >> operator and manages the input coming in through the input stream
+     * 
+     * @param is the input stream
+     * @param i the integer being inputed
+     * @return returns a std::istream& input stream
+     */
     std::istream& operator>>(std::istream& is, Integer& i)
     {
         std::string val;
@@ -553,6 +811,11 @@ namespace cosc326
         return is;
     }
 
+    /**
+     * @brief The toString() function returns the integer as a string
+     * 
+     * @return returns a std::string 
+     */
     std::string Integer::toString() const
     {
         return value;
